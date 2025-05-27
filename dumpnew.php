9a6a5827233d02a988b51a77adfb6b5543687306
Wawan R <smallfolks@users.noreply.github.com>
2025-05-27 15:02:24 +0700

    Update dumpnew.php

--- dumpnew.php
+++ dumpnew.php
@@ -1,23 +1,48 @@
-d<?php
+<?php
 // Konfigurasi database
 $host = "127.0.0.1";
 $user = "u963859540_siqurban";
 $pass = "u963859540_siqurban";
 $db   = "u963859540_siqurban";
 
-// Nama file backup
-$backup_file = "backup_" . $db . "_" . date("Ymd_His") . ".sql";
-
-// Jalankan mysqldump lewat shell
-$command = "mysqldump -h $host -u $user -p$pass $db > $backup_file";
-
-// Eksekusi perintah
-system($command, $output);
-
-// Cek hasil
-if (file_exists($backup_file)) {
-    echo "Backup berhasil: <a href='$backup_file'>$backup_file</a>";
-} else {
-    echo "Backup gagal.";
+// Koneksi
+$conn = new mysqli($host, $user, $pass, $db);
+if ($conn->connect_error) {
+    die("Koneksi gagal: " . $conn->connect_error);
 }
-?>
+
+// Nama file yang akan diunduh
+$filename = "backup_{$db}_" . date("Ymd_His") . ".sql";
+
+// Header agar langsung download
+header("Content-Type: application/sql");
+header("Content-Disposition: attachment; filename=\"$filename\"");
+
+$output = "";
+
+// Backup struktur dan data
+$tables = [];
+$res = $conn->query("SHOW TABLES");
+while ($row = $res->fetch_array()) {
+    $tables[] = $row[0];
+}
+
+foreach ($tables as $table) {
+    // Struktur tabel
+    $res = $conn->query("SHOW CREATE TABLE `$table`");
+    $row = $res->fetch_assoc();
+    $output .= "-- Struktur tabel `$table`\n";
+    $output .= $row['Create Table'] . ";\n\n";
+
+    // Data tabel
+    $res = $conn->query("SELECT * FROM `$table`");
+    while ($r = $res->fetch_assoc()) {
+        $vals = array_map([$conn, 'real_escape_string'], array_values($r));
+        $vals = array_map(fn($v) => "'" . $v . "'", $vals);
+        $output .= "INSERT INTO `$table` VALUES (" . implode(", ", $vals) . ");\n";
+    }
+    $output .= "\n\n";
+}
+
+echo $output;
+exit;
