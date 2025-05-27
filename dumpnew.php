bddb43f23db73c1f4af4806d10a486a09643fa03
Wawan R <smallfolks@users.noreply.github.com>
2025-05-27 15:24:39 +0700

    Update dumpnew.php

--- dumpnew.php
+++ dumpnew.php
@@ -1,7 +1,4 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
 
 $host = '127.0.0.1';
 $username = 'u963859540_siqurbanasyifa'; // ganti dengan benar
