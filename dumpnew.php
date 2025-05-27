68e33abe3d6f8bad6227c200cd4385fe7ecba175
Wawan R <smallfolks@users.noreply.github.com>
2025-05-27 15:13:40 +0700

    Update dumpnew.php

--- dumpnew.php
+++ dumpnew.php
@@ -1,24 +1,29 @@
 <?php
-header("Content-Type: application/sql");
-header("Content-Disposition: attachment; filename=\"backup_" . date("Ymd_His") . ".sql\"");
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
 
-$mysqli = new mysqli("127.0.0.1", "u963859540_siqurban_mbrkh", "u963859540_siqurban_mbrkh", "u963859540_siqurban_mbrkh");
-if ($mysqli->connect_error) die("Koneksi gagal");
+$mysqli = new mysqli("localhost", "u963859540_siqurban_mbrkh", "u963859540_siqurban_mbrkh", "u963859540_siqurban_mbrkh");
+if ($mysqli->connect_error) die("Koneksi gagal: " . $mysqli->connect_error);
 
-$res = $mysqli->query("SHOW TABLES");
-while ($row = $res->fetch_array()) {
-    $table = $row[0];
+$table = 'nama_tabelmu'; // Ganti dengan nama tabel nyata
 
-    $create = $mysqli->query("SHOW CREATE TABLE `$table`")->fetch_assoc();
-    echo "-- Struktur untuk $table\n";
-    echo $create['Create Table'] . ";\n\n";
+header('Content-Type: text/csv');
+header("Content-Disposition: attachment; filename=\"$table.csv\"");
 
-    $data = $mysqli->query("SELECT * FROM `$table`");
-    while ($r = $data->fetch_assoc()) {
-        $vals = array_map([$mysqli, 'real_escape_string'], array_values($r));
-        $vals = array_map(fn($v) => "'$v'", $vals);
-        echo "INSERT INTO `$table` VALUES (" . implode(",", $vals) . ");\n";
-    }
-    echo "\n";
+$output = fopen("php://output", "w");
+
+// Ambil kolom header
+$res = $mysqli->query("SELECT * FROM `$table` LIMIT 1");
+if ($res) {
+    $fields = array_keys($res->fetch_assoc());
+    fputcsv($output, $fields);
 }
-?>
+
+// Ambil semua data
+$res = $mysqli->query("SELECT * FROM `$table`");
+while ($row = $res->fetch_assoc()) {
+    fputcsv($output, $row);
+}
+
+fclose($output);
+exit;
